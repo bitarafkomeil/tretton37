17fdad5b769eaaf29ccfa2e3fefbdaca481548a6
k.bitaraf <komeil1612@b>
2022-07-04 08:54:07 -0700

    Initialize Project

---
+++ src/main/java/com/tretton37/Tretton37Application.java
@@ -0,0 +1,13 @@
+package com.tretton37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tretton37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tretton37Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tretton37/Tretton37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tretton37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tretton37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
